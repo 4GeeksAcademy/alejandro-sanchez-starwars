3eaeddbe046986882ea7a8062a25775d3a0b3ab7
Alejosz1902 <111547538+Alejosz1902@users.noreply.github.com>
2024-08-22 20:51:31 +0000

    Project completed

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Characters from "../component/characters.jsx";
+import Vehicles from "../component/vehicles.jsx";
+import Planets from "../component/planets.jsx";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+  <div className="text-center mt-5">
+    <div className="my-5">
+      <Characters />
+    </div>
+    <div className="my-5">
+      <Planets />
+    </div>
+    <div className="my-5">
+      <Vehicles />
+    </div>
+  </div>
 );
